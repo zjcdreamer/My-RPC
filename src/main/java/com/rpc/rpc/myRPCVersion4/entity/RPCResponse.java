afa2e642285eab7bafa7b25734dcc45f412887c4
Zhangjincheng <382462237@qq.com>
2021-06-27 17:45:12 +0800

    修复Version中DataType的空指针bug

--- src/main/java/com/rpc/rpc/myRPCVersion4/entity/RPCResponse.java
+++ src/main/java/com/rpc/rpc/myRPCVersion4/entity/RPCResponse.java
@@ -19,7 +19,7 @@ public class RPCResponse implements Serializable {
     private Object data;
 
     public static RPCResponse success(Object data) {
-        return RPCResponse.builder().code(200).data(data).build();
+        return RPCResponse.builder().code(200).data(data).dataType(data.getClass()).build();
     }
     public static RPCResponse fail() {
         return RPCResponse.builder().code(500).message("服务器发生错误").build();
